9eeab9f0115630c23bae6ca7e43813d0eaddf128
daveoc90 <david_oconnor@live.ie>
2018-06-26 10:09:57 -0400

    fix wrong variable

--- cpm.py
+++ cpm.py
@@ -38,12 +38,12 @@ def train_cpm(ipmat,pheno):
     ne=ne.sum(axis=0)/2
 
     if np.sum(pe) > 0:
-        fit_pos=np.polyfit(pe,pmatvals,1)
+        fit_pos=np.polyfit(pe,pheno,1)
     else:
        fit_pos=[]
 
     if np.sum(ne) > 0:
-        fit_neg=np.polyfit(ne,pmatvals,1)
+        fit_neg=np.polyfit(ne,pheno,1)
     else:
        fit_neg=[]
 
