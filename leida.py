4588c2c988d854ccf6e85bed560ba984fc46d686
daveoc90 <6143678+DaveOC90@users.noreply.github.com>
2018-07-23 15:30:59 -0400

    code structure update

--- leida.py
+++ leida.py
@@ -5,6 +5,9 @@ from scipy import signal as sg
 from matplotlib import pyplot as plt
 import seaborn as sns
 import sklearn as skl
+from sklearn import skl,svm,cluster
+import pandas as pd
+import glob
 
 
 def cosine_similarity(timeseries):
@@ -86,7 +89,7 @@ def tsfilt(timeseries):
 
 
 def dotnorm(v1,v2):
-    return  np.dot(v1,v2)/np.linalg.norm(v1)/np.linalg.norm(v2)	
+    return  np.dot(v1,v2)/np.linalg.norm(v1)/np.linalg.norm(v2)   
 
 def indv_leida_mats(onesubdata,numeigs=1):
     """
@@ -166,7 +169,89 @@ def testcode():
     plt.show()
 
 
-    def kcluster():
-    	est=sk.clusters.Kmean(n_clusters=11)
-    	est.fit()
+def kcluster_train(ipfeatures,nclust):
+    est=cluster.KMeans(n_clusters=nclust)
+    est.fit(ipfeatures)
 
+    return est
+
+
+
+def moody_data_split():
+    mats=glob.glob("/mnt/store1/mridata2/mri_group/HCP_data/HCP_900_DATA/REST_LR/matrices/*REST*LR*_GSR*roimean.txt")
+
+    pmatdf=pd.read_csv('/home/dmo39/pmatfilter.csv')
+    substouse=list(map(str, pmatdf.Subject.values))
+
+    matstouse=sorted([m for m in mats if any(s in m for s in substouse)])
+
+    imnets_load=io.loadmat('/mnt/store2/mri_group/dave_data/code/LEiDA-master/LEiDA/netrank.mat')
+    imnets=imnets_load['imnets']
+
+    split_ts(matstouse,imnets,'/mnt/store2/mri_group/dave_data/code/LEiDA-master/LEiDA/NetParcelCorrMats_HCP.mat')
+
+
+def split_ts(ipmats, statelbls, savepath):
+    #mats=glob.glob("/mnt/store1/mridata2/mri_group/HCP_data/HCP_900_DATA/REST_LR/matrices/*REST*LR*_GSR*roimean.txt")
+
+    #mats=glob.glob("/mnt/store1/*roimean.txt")
+
+    matnames=[m.split('/')[-1] for m in ipmats]
+
+    roidfs=[pd.read_csv(m,index_col=0,sep='\t').dropna(axis=1) for m in ipmats]
+
+    data_dct={}
+
+    for netnum in np.unique(statelbls):
+        group_list=[]    
+
+        for dfnum,roidf in enumerate(roidfs):
+
+            print('Network ',netnum, 'Subject', dfnum)
+
+            imnet=imnets[:,dfnum]
+            subdf=roidf[imnet==netnum]
+            corrmat=subdf.corr().values
+            group_list.append(corrmat)
+
+        netmats=np.array(group_list)
+
+        data_dct['Network'+str(netnum)]=netmats
+
+
+    #io.savemat('/mnt/store2/mri_group/dave_data/code/LEiDA-master/LEiDA/NetParcelCorrMats_HCP.mat',data_dct)
+    io.savemat(savepath,data_dct)
+
+        
+def svr_():
+
+    imnets_load=io.loadmat('/mnt/store2/mri_group/dave_data/code/LEiDA-master/LEiDA/netrank.mat')
+    imnets=imnets_load['imnets']
+
+    pmatdf=pd.read_csv('/home/dmo39/pmatfilter.csv')
+    excludevals=pmatdf[pmatdf.PMAT24_A_CR.isna()].index.values
+    pmatdf_filt=pmatdf.dropna()
+    pmatvals=pmatdf_filt.PMAT24_A_CR.values
+
+
+
+
+
+    features=np.array([np.histogram(ims,bins=11)[0] for ims in imnets.T])
+
+    svr=svm.SVR()
+
+    param_grid = [
+      {'C': [1, 10, 100, 1000], 'kernel': ['linear']},
+      {'C': [1, 10, 100, 1000], 'gamma': [0.001, 0.0001], 'kernel': ['rbf']},
+     ]
+
+    clf = GridSearchCV(svr, param_grid, cv=5)
+
+    clf.fit(features_filt_train,pmatvals_train)
+
+    includevals=list(set(np.arange(0,865))-set(excludevals))
+
+    features_filt=features[includevals,:]
+
+    np.corrcoef(clf.predict(features_filt[401:,:]),pmatvals[401:])
