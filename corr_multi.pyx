a00b224837c3a5884ef3a8e330a0364195624236
DaveOC90 <6143678+DaveOC90@users.noreply.github.com>
2019-09-16 11:17:50 -0400

    first attempt at a cython partial correlation function

--- corr_multi.pyx
+++ corr_multi.pyx
@@ -52,3 +52,50 @@ def corr_multi_cy(arr,mat):
         coll[n]=(r)
 
     return coll
+
+def partial_corr(x,y,z):
+
+    # Z is independent variable with shared influence on x and y
+
+    numel = int(len(y))
+
+    print(numel)
+
+    ymean = bn.nanmean(y)
+    zmean = bn.nanmean(z)
+    ydemean = y - ymean
+    zdemean = z - zmean
+
+    num_yz = zdemean * ydemean
+    den_yz = bn.ss(zdemean)
+    den_xz = den_yz
+
+    b1_yz = num_yz/den_yz
+    b0_yz = ymean - (b1_yz * zmean)
+
+    ypred = b0_yz + b1_yz * z
+    yres = y - ypred
+
+    numcompare = len(x.T)
+
+    coll=ndarray(numcompare)
+
+    for n in range(0,numcompare):
+        subx=x[:,n]
+
+        xmean=bn.nanmean(subx)
+        xdemean=subx-xmean
+        
+        num_xz = zdemean * xdemean
+        
+        b1_xz = num_xz/den_xz
+        b0_xz = xmean - (b1_xz * zmean)
+
+        xpred = b0_xz + b1_xz * z
+        xres = x - xpred
+       
+        coll[n]=corr_multi_cy(xres,yres)
+
+    return coll
+
+
